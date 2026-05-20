2278a3ea2c7807bdf95edebee44a2f26603b99c1
Mark Justin <paquerajustin@gmail.com>
2026-05-20 11:31:07 +0800

    Fix email timeout issue

--- _helpers/send-email.ts
+++ _helpers/send-email.ts
@@ -1,7 +1,16 @@
 import nodemailer from 'nodemailer';
 import config from '../config.json';
-
 export default async function sendEmail({ to, subject, html, from = config.emailFrom }: any) {
-  const transporter = nodemailer.createTransport(config.smtpOptions);
-  await transporter.sendMail({ from, to, subject, html });
+  const transporter = nodemailer.createTransport({
+    ...config.smtpOptions,
+    connectionTimeout: 10000,
+    greetingTimeout: 10000,
+    socketTimeout: 10000
+  });
+  try {
+    await transporter.sendMail({ from, to, subject, html });
+  } catch (err) {
+    console.error('Email sending failed:', err);
+    // Don't throw - allow registration to succeed even if email fails
+  }
 }
