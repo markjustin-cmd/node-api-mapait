715327e97838197f339cb31fdc50f8b6b0fb42e8
Mark Justin <paquerajustin@gmail.com>
2026-05-21 15:01:04 +0800

    Use environment variables for SMTP

--- _helpers/send-email.ts
+++ _helpers/send-email.ts
@@ -1,13 +1,14 @@
 import nodemailer from 'nodemailer';
 import config from '../config.json';
+
 export default async function sendEmail({ to, subject, html, from = config.emailFrom }: any) {
   const transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
     port: 465,
     secure: true,
     auth: {
-      user: config.smtpOptions.auth.user,
-      pass: config.smtpOptions.auth.pass
+      user: process.env.SMTP_USER || config.smtpOptions.auth.user,
+      pass: process.env.SMTP_PASS || config.smtpOptions.auth.pass
     }
   });
   try {
